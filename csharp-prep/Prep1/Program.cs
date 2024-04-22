059c4d23f8154162abf57a61d0ffe07af08237d2
Samuel Mercado <smercadoperez67@gmail.com>
2024-04-22 16:27:31 -0400

    Finished Prep 1 task

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,10 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello CSE 210 World! ");
+        Console.WriteLine("What is your first name?");
+        string firstName = Console.ReadLine();
+        Console.WriteLine("What is your last name?");
+        string lastName = Console.ReadLine();
+        Console.Write($"Your name is {lastName}, {firstName} {lastName}");
     }
 }
