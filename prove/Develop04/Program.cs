ed508540c82be2677c0249c69c3d0bbc9899606d
Samuel Mercado <smercadoperez67@gmail.com>
2024-05-16 22:28:37 -0400

    still working on this

--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -4,6 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop04 World!");
+        bool sam = true;
+
+        while (sam)
+        {
+            Console.Write("+");
+
+            Thread.Sleep(500);
+
+            Console.Write("\b \b"); // Erase the + character
+            Console.Write("-"); // Replace it with the - character
+        }
+
     }
 }
